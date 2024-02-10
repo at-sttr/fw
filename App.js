38ffb7e51ef905107b84040cb91ef8e266c3f1ca
T1234561 <159491551+T1234561@users.noreply.github.com>
2024-02-10 08:37:29 +0100

    Add files via upload
    Updating Styles

--- App.js
+++ App.js
@@ -110,7 +110,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     width: '100%',
-    height: '100%'
+    aspectRatio: 10 / 3
   },
   button: {
     marginBottom:20,
